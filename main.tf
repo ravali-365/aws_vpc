d3b8ba9b9dfd45dfa7ad6dc9745a3e5f5a5d2c8d
George VM <georgeemail.gmail.com>
2025-03-08 17:05:20 +0000

    Add public subnets in main.tf
    What:
    - Created three public subnets across multiple AZs.
    - Enabled automatic public IP assignment for instances.
    
    Why:
    - Ensures availability and fault tolerance for internet-facing resources.
    - Facilitates external access to public resources.
    
    How:
    - Uses CIDR blocks from 'public_subnets_cidr' variable.

--- main.tf
+++ main.tf
@@ -1,3 +1,10 @@
 resource "aws_vpc" "my_vpc" {
   cidr_block = var.vpc_cidr
 }
+
+resource "aws_subnet" "public_subnet" {
+  count                  = 3
+  vpc_id                 = aws_vpc.my_vpc.id
+  cidr_block             = var.public_subnets_cidr[count.index]
+  map_public_ip_on_launch = true
+}
