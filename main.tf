901c2ff29e0cc6005e6dc23a30c71d044c558a36
George VM <georgeemail.gmail.com>
2025-03-08 17:05:59 +0000

    Add private subnets in main.tf
    What:
    - Created three private subnets across multiple AZs.
    - Prevented public IP assignment for security.
    
    Why:
    - Ensures isolation for backend resources without direct internet access.
    - Supports private communication within the VPC.
    
    How:
    - Uses CIDR blocks from 'private_subnets_cidr' variable.

--- main.tf
+++ main.tf
@@ -8,3 +8,10 @@ resource "aws_subnet" "public_subnet" {
   cidr_block             = var.public_subnets_cidr[count.index]
   map_public_ip_on_launch = true
 }
+
+
+resource "aws_subnet" "private_subnet" {
+  count      = 3
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = var.private_subnets_cidr[count.index]
+}
