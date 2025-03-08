c5864255f8271b06228a2eb88878f597904f2710
George VM <georgeemail.gmail.com>
2025-03-08 17:02:37 +0000

    Define variables for public and private subnets CIDR blocks
    What:
    - Added 'public_subnets_cidr' and 'private_subnets_cidr' variables.
    - Default values set for three public and three private subnets.
    
    Why:
    - Facilitates customization of subnet IP ranges.
    - Ensures isolation between public and private resources.
    
    How:
    - Used in main.tf to create subnets dynamically.

--- variables.tf
+++ variables.tf
@@ -2,3 +2,16 @@ variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   default     = "10.0.0.0/16"
 }
+
+variable "public_subnets_cidr" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+
+variable "private_subnets_cidr" {
+  description = "CIDR blocks for private subnets"
+  type        = list(string)
+  default     = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+}
+
