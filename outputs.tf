e7ed5e759f24859105724a3255120d31d96b556d
George VM <georgeemail.gmail.com>
2025-03-08 17:10:57 +0000

    Add outputs for subnet IDs and EC2 public IP
    What:
    - Added output variables for public and private subnet IDs.
    - Created output for public IP of EC2 instance.
    
    Why:
    - Simplifies access to key resource information post-deployment.
    - Facilitates quick testing of HTTP access to EC2.
    
    How:
    - Uses resource IDs and public IP from main.tf.

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,15 @@
 output "vpc_id" {
   value = aws_vpc.my_vpc.id
 }
+
+output "public_subnets" {
+  value = aws_subnet.public_subnet[*].id
+}
+
+output "private_subnets" {
+  value = aws_subnet.private_subnet[*].id
+}
+
+output "web_server_public_ip" {
+  value = aws_instance.web_server.public_ip
+}
