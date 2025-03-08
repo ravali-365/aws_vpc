b18848447dd3421cf582475aebcda0446af0f045
George VM <georgeemail.gmail.com>
2025-03-08 17:07:21 +0000

    Configure Internet Gateway and route table for public subnets
    What:
    - Added 'aws_internet_gateway' for outbound internet access.
    - Created route table to route traffic to IGW for public subnets.
    
    Why:
    - Essential for public-facing resources to access the internet.
    - Enables secure and controlled traffic flow.
    
    How:
    - Associated route table with public subnets only.

--- main.tf
+++ main.tf
@@ -15,3 +15,23 @@ resource "aws_subnet" "private_subnet" {
   vpc_id     = aws_vpc.my_vpc.id
   cidr_block = var.private_subnets_cidr[count.index]
 }
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.my_vpc.id
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.my_vpc.id
+}
+
+resource "aws_route" "public_route" {
+  route_table_id         = aws_route_table.public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  count          = 3
+  subnet_id      = aws_subnet.public_subnet[count.index].id
+  route_table_id = aws_route_table.public_rt.id
+}
